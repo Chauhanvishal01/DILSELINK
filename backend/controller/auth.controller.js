6887c22a6597d5aed4ea8caa24d5becc9ea9628c
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-26 22:12:08 +0530

    Add login and logout functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -45,7 +45,45 @@ export const register = async (req, res) => {
     }
   } catch (error) {
     console.log("Error from register", error.message);
+    return res.status(500).json({ error: "Internal Server Error." });
+  }
+};
 
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password." });
+    }
+    generateTokenAndSetCookie(user._id, res);
+    return res.status(201).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    });
+  } catch (error) {
+    console.log("Error from login", error.message);
+    return res.status(500).json({ error: "Internal Server Error." });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "User logged out" });
+  } catch (error) {
+    console.log("Error from logout", error.message);
     return res.status(500).json({ error: "Internal Server Error." });
   }
 };
