d6a2a3b684ffe5855a1b64f82972ec71ffef8b8d
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-27 14:14:20 +0530

    suggested user route and the update user profile route and functionality

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -2,12 +2,16 @@ import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import {
   followUnfollowUser,
+  getSuggestion,
   getUserProfile,
+  updateUserProfile,
 } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", authMiddleware, getUserProfile);
+router.get("/suggestedUsers", authMiddleware, getSuggestion);
 router.post("/follow/:id", authMiddleware, followUnfollowUser);
+router.post("/update", authMiddleware, updateUserProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,18 @@ import cookieParser from "cookie-parser";
 import dbConnection from "./database/db.connection.js";
 import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users.routes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
-
 const app = express();
+
+///Cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
