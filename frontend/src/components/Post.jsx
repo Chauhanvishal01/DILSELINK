accddb78a5879b4d2c809398db85ae454e7db0e0
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-29 10:57:06 +0530

    Like and Unlike functinality

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -12,7 +12,7 @@ const Post = ({ post }) => {
   const [comment, setComment] = useState("");
   const { data } = useQuery({ queryKey: ["authUser"] });
   const queryClient = useQueryClient();
-  const { mutate: deletePost, isPending } = useMutation({
+  const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
       try {
         const res = await fetch(`/api/v1/posts/delete/${post._id}`, {
@@ -34,14 +34,45 @@ const Post = ({ post }) => {
     },
   });
 
+  const { mutate: likePost, isPending: isLiking } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/v1/posts/like/${post._id}`, {
+          method: "POST",
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (updatedLikes) => {
+      queryClient.setQueryData(["posts"], (oldData) => {
+        return oldData.map((oldPost) => {
+          if (oldPost._id === post._id) {
+            return { ...oldPost, likes: updatedLikes };
+          }
+          return oldPost;
+        });
+      });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   const postOwner = post.user;
-  const isLiked = false;
+  const isLiked = Array.isArray(post.likes) && post.likes.includes(data._id);
 
   const isMyPost = data._id === post.user._id;
 
   const formattedDate = "1h";
 
-  const isCommenting = false;
+  const isCommenting = true;
 
   const handleDeletePost = () => {
     deletePost();
@@ -51,7 +82,10 @@ const Post = ({ post }) => {
     e.preventDefault();
   };
 
-  const handleLikePost = () => {};
+  const handleLikePost = () => {
+    if (isLiking) return;
+    likePost();
+  };
 
   return (
     <>
@@ -78,13 +112,13 @@ const Post = ({ post }) => {
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                {!isPending && (
+                {!isDeleting && (
                   <FaTrash
                     className="cursor-pointer hover:text-red-500"
                     onClick={handleDeletePost}
                   />
                 )}
-                {isPending && <Spinner />}
+                {isDeleting && <Spinner />}
               </span>
             )}
           </div>
@@ -113,7 +147,6 @@ const Post = ({ post }) => {
                   {post.comments.length}
                 </span>
               </div>
-              {/* We're using Modal Component from DaisyUI */}
               <dialog
                 id={`comments_modal${post._id}`}
                 className="modal border-none outline-none"
@@ -185,19 +218,19 @@ const Post = ({ post }) => {
                 className="flex gap-1 items-center group cursor-pointer"
                 onClick={handleLikePost}
               >
-                {!isLiked && (
-                  <FaRegHeart className="w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500" />
+                {!isLiked && !isLiking && (
+                  <FaRegHeart className="w-4 h-4 cursor-pointer  group-hover:text-pink-500" />
                 )}
-                {isLiked && (
+                {isLiked && !isLiking && (
                   <FaRegHeart className="w-4 h-4 cursor-pointer text-pink-500 " />
                 )}
 
                 <span
                   className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-                    isLiked ? "text-pink-500" : ""
+                    isLiked ? "text-pink-500" : "text-slate-500"
                   }`}
                 >
-                  {post.likes.length}
+                  {post.likes.length || 0}
                 </span>
               </div>
             </div>
