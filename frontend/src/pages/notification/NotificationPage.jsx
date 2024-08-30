12ff253ae49c17d5368bc6e91f93ed425051d844
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-30 05:51:12 +0530

    Solve small Issue

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -3,33 +3,45 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 import Spinner from "../../components/subComponents/Spinner";
-
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 const NotificationPage = () => {
-  const isLoading = false;
-  const notifications = [
-    {
-      _id: "1",
-      from: {
-        _id: "1",
-        username: "johndoe",
-        profileImg: "/man2.jpg",
-      },
-      type: "follow",
+  const queryClient = useQueryClient();
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ["notification"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("api/v1/notifications/");
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
-    {
-      _id: "2",
-      from: {
-        _id: "2",
-        username: "janedoe",
-        profileImg: "/woman1.png",
-      },
-      type: "like",
-    },
-  ];
+  });
 
-  const deleteNotifications = () => {
-    alert("All notifications deleted");
-  };
+  const { mutate: deleteNotifications } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/v1/notifications/delete", {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: (data) => {
+      toast.success(data.message || "deleted");
+      queryClient.invalidateQueries({ queryKey: ["notification"] });
+    },
+    onError: (error) => {
+      toast.error(error.message || "something wrong");
+    },
+  });
 
   return (
     <>
@@ -45,7 +57,7 @@ const NotificationPage = () => {
               className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a onClick={deleteNotifications}>Delete all notifications</a>
+                <a onClick={deleteNotifications}>Clear all notifications</a>
               </li>
             </ul>
           </div>
@@ -70,12 +82,7 @@ const NotificationPage = () => {
               <Link to={`/profile/${notification.from.username}`}>
                 <div className="avatar">
                   <div className="w-8 rounded-full">
-                    <img
-                      src={
-                        notification.from.profileImg ||
-                        "/avatar-placeholder.png"
-                      }
-                    />
+                    <img src={notification.from.profileImg || "/avatar.png"} />
                   </div>
                 </div>
                 <div className="flex gap-1">
