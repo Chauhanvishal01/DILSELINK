436c700c3ff0229522519f9192b14e4734fb30e7
Vishal Chauhan <vishal2825m@gmail.com>
2024-09-29 11:24:23 +0530

    Install Recoil

--- frontend/src/utils/messages.js
+++ frontend/src/utils/messages.js
@@ -1,9 +1,28 @@
+import { atom } from "recoil";
+
+export const conversationsAtom = atom({
+	key: "conversationsAtom",
+	default: [],
+});
+
+export const selectedConversationAtom = atom({
+	key: "selectedConversationAtom",
+	default: {
+		_id: "",
+		userId: "",
+		username: "",
+		userProfilePic: "",
+	},
+});
+
+
+
 export const fetchConversations = async () => {
-    const res = await fetch("/api/v1/messages/conversation");
-    if (!res.ok) {
-      const errorData = await res.json();
-      throw new Error(errorData.error || "Error fetching conversations");
-    }
-    return res.json();
-  };
-  
+  const res = await fetch("/api/v1/messages/conversation");
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.error || "Error fetching conversations");
+  }
+  return res.json();
+};
+
