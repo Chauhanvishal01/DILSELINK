0ab49a4a2d5fb439d7d73ba826d279e13bae503d
Vishal Chauhan <vishal2825m@gmail.com>
2024-09-29 11:26:19 +0530

    Recoil in main .jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+
 import { SocketContextProvider } from "./context/SocketContext.jsx";
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,12 +17,14 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <SocketContextProvider>
-          <App />
-        </SocketContextProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </StrictMode>
 );
